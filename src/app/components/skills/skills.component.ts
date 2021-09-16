a917b7af07859ea6f938e3f73258ed4352d1114c
Racette <olivier.racette@cgi.com>
2021-09-16 11:44:26 -0400

    knowledge section

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -7,8 +7,6 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-  skills: string[] = ["Python", "C++", "C#", "Java", "Javascript", "Typescript", "HTML/CSS" , "SQL" , "Unity", "Godot", "GLSL"];
-
   constructor(private translate: TranslateService) { }
 
   ngOnInit(): void {
